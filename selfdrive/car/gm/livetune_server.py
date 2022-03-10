fcf1844e1ad707169d4f03f510fed68611a7cfca
Jason Shuler <jshuler@gmail.com>
2022-03-09 23:29:49 -0500

    may return CP

--- selfdrive/car/gm/livetune_server.py
+++ selfdrive/car/gm/livetune_server.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 import threading
 from wsgiref.simple_server import WSGIServer, make_server
-#import json
+
+#from cereal import car
+import json
 
 # POC / example web service for live tuning
 # TODO: auth, use an existing restful library
 # TODO: make the code not suck
+# TODO: not using a class here makes me feel kinda sick...
 
-exampleResp = b"{\"minEnableSpeed\": 0, \"steerRatio\": 15.2}"
-
-started = False
 httpd : WSGIServer
 th : threading.Thread
+global _CP
+# TODO: May need to do this the right way and create a service with events to be reacted to...
 
 def app(environ, start_response):
+    global _CP
+    if _CP is None:
+        start_response('200 OK', [('Content-Type', 'text/json')])
+        return [b"Whoopsie no _CP"]
     # if environ['REQUEST_METHOD'] == 'POST':
     #     start_response('200 OK', [('Content-Type', 'text/json')])
     #     return [b'']
+    data = json.dumps(_CP)
     start_response('200 OK', [('Content-Type', 'text/json')])
-    return [exampleResp]
+    return [data.encode()]
 
-def launch_listener_async():
-    if (started):
-        return
+def launch_listener_async(CP):
+    global _CP
+    #if (httpd is not None):
+    #    return
+    #started = True
+    _CP = CP
     th = threading.Thread(target=_launch_listener)
     th.start()
 
